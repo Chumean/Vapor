52cb71dd99fa35793313a5312466d9a64860ad2f
david <david.chaotic.chu@gmail.com>
2023-05-24 00:49:28 -0400

    add review model

---
+++ app/models/review.py
@@ -0,0 +1,28 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import Column, ForeignKey, Table
+from sqlalchemy.types import Integer, String, Boolean
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(250))
+    recommended = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+    game_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("games.id")))
+
+    owner = db.relationship("User", back_populates="review")
+    game = db.relationship("Game", back_populates='review')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'review': self.review,
+            'recommended': self.recommended,
+            'user_id': self.user_id,
+            'game_id': self.game_id
+        }
