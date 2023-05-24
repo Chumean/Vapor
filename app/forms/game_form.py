8d4c11863533c261d6baa202006fd0c04f25ee2a
david <david.chaotic.chu@gmail.com>
2023-05-24 00:41:10 -0400

    add game_form

---
+++ app/forms/game_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField, SubmitField, TextAreaField, DateField
+from wtforms.validators import DataRequired
+
+
+class GameForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    genre = StringField('Genre', validators=[DataRequired()])
+    developer = StringField('Developer', validators=[DataRequired()])
+    publisher = StringField('Publisher', validators=[DataRequired()])
+    franchise = StringField('Franchise', validators=[DataRequired()])
+    release_date = DateField('Release_date', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    tags = StringField("Tags", validators=[DataRequired()])
+    image = StringField("Image", validators=[DataRequired()])
