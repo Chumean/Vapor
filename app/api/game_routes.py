c2bb55763f882acebc6ce3bc66e87bd049a974a7
david <david.chaotic.chu@gmail.com>
2023-05-25 00:53:01 -0400

    add review routes

--- app/api/game_routes.py
+++ app/api/game_routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify, session, request, abort, redirect, url_for
-from app.models import Game, db
+from app.models import Game, db, Review
 from app.forms.game_form import GameForm
+from app.forms.review_form import ReviewForm
 from flask_login import login_required, current_user
 
 game_routes = Blueprint('games', __name__,)
 
+# GET ALL GAMES
 @game_routes.route('/')
 def get_all_games():
     games = Game.query.all()
@@ -13,3 +15,41 @@ def get_all_games():
         game_dict = game.to_dict()
         game_list.append(game_dict)
     return jsonify({'games': game_dict})
+
+# GET GAME DETAILS
+@games_routes.route('/<int:id>')
+def get_game_details():
+    game = Game.query.get(id)
+    if not game:
+        return jsonify({'error': 'Game not found'}), 404
+    return jsonify(game.to_dict())
+
+# GET REVIEWS
+@game_routes.route('/<int:id>/reviews', methods=["GET"])
+def get_reviews(id):
+    reviews = Review.query.filter_by(game_id=id).all()
+    return jsonify([review.to_dict() for review in reviews])
+
+# ADD REVIEW
+@game_routes.route('/<int:id>/reviews', methods={"POST"})
+@login_required
+def add_review(id):
+    form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    review = form.review.data
+    recommended = form.recommended.data
+    user_id = form.user_id.data
+    game_id = id
+
+    new_review = Review(
+        review=review,
+        recommended=recommended,
+        user_id=user_id,
+        game_id=game_id
+    )
+
+    db.session.add(new_review)
+    db.session.commit()
+
+    return new_review.to_dict();
---
+++ app/api/review_routes.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, jsonify, session
+from flask_login import login_required, current_user
+from app.models import Review, db
+from app.forms.review_form import ReviewForm
+
+review_routes = Blueprint('reviews', __name__)
+
+# DELETE REVIEW
+@review_routes.route('/<int:id>', methods=["DELETE"])
+@login_required
+def delete_review(id):
+
+    user_id = current_user.id
+
+    review = Review.query.get(id)
+
+    if not review or review.user_id != user_id:
+        return jsonify({'error':'Review not found or unauthorized'}, 404)
+
+    db.session.delete(review)
+    db.session.commit()
+
+    return review.to_dict()
+
+# EDIT REVIEW
+@review_routes.route('/<int:id>,', methods=["PUT"])
+@login_required
+def edit_review(id):
+
+    user_id = current_user.id
+
+    review = Review.query.get(id)
+
+    if not review or review.user_id != user_id:
+        return jsonify({'error':'Review not found or unauthorized'}, 404)
+
+    form = ReviewForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        review = form.review.data
+        recommended = form.recommended.data
+        user_id = form.user_id.data
+        game_id = form.game_id.data
+
+        db.session.commit()
+
+        return review.to_dict()
+
+    return "NO EDIT"
