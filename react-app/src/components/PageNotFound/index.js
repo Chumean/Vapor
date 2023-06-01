2f15ea20706d1f3f610877d7e4697b637310f369
david <david.chaotic.chu@gmail.com>
2023-06-01 01:07:42 -0400

    add base components without code

---
+++ react-app/src/components/PageNotFound/index.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h2>The page you are trying to visit is not valid</h2>
+            <h3>Please go back to the homepage</h3>
+        </div>
+    )
+}
