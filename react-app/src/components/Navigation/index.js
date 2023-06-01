21c84eeb9a955968fe32bbcfb2d7b42e48721eaa
david <david.chaotic.chu@gmail.com>
2023-05-31 22:06:09 -0400

    add css landing page

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,21 +3,106 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import { FaDownload } from 'react-icons/fa'
+import logo from "../../assets/logo.png"
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
+		// <div className='nav-background'>
+		// 	<div className='nav-container'>
+		// 		<div className='nav-logo'>
+
+
+		// 			<div className='vapor-logo'>
+        //             	<img src={logo} style={{ width: '3rem', height: '3rem', marginRight: '0.5rem' }} />
+        //             	<p className='vapor-text'>VAPOR</p>
+        //         	</div>
+
+		// 		</div>
+
+		// 		<div className='middle-nav'>
+        //         	<ul className='middle-list'>
+        //             	<li>
+        //                 	<p>SHOP</p>
+        //             	</li>
+
+        //             	<li>
+        //                 	<p>COMMUNITY</p>
+        //             	</li>
+
+        //             	<li>
+        //                 	<p>ABOUT</p>
+        //             	</li>
+
+        //             	<li>
+        //                 	<p>SUPPORT</p>
+        //             	</li>
+        //         	</ul>
+        //     	</div>
+
+		// 		<div className='right-nav'>
+        //         	<div className='install-logo'>
+        //             	<FaDownload />
+        //             	<p className='install-vapor'>Install Vapor</p>
+        //         	</div>
+
+        //         	<div className='login-area'>
+        //             	<p className='login-text'>login</p>
+        //         	</div>
+
+        //         	<div className='login-separate'></div>
+
+        //         	<div className=''>
+        //             	<p className='language-text'>language</p>
+        //         	</div>
+        //     	</div>
+		// 	<ul>
+		// 	<li>
+		// 		<NavLink exact to="/">Home</NavLink>
+		// 	</li>
+		// 	{isLoaded && (
+		// 		<li>
+		// 			<ProfileButton user={sessionUser} />
+		// 		</li>
+		// 	)}
+		// 	</ul>
+		// 	</div>
+		// </div>
+
+		<header className="header">
+		<div className="logo">
+		  <img src={logo} alt="Vaporlogo" className="logo-img" />
+		  <p className='vapor-text'>VAPOR</p>
+		</div>
+		<nav className="header-nav">
+		  <ul className="nav-list">
+			<li className="nav-item">
+			  <a href="#" className="nav-link">STORE</a>
 			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+			<li className="nav-item">
+			  <a href="#" className="nav-link">COMMUNITY</a>
+			</li>
+			<li className="nav-item">
+			  <a href="#" className="nav-link">ABOUT</a>
+			</li>
+			<li className="nav-item">
+			  <a href="#" className="nav-link">SUPPORT</a>
+			</li>
+		  </ul>
+		</nav>
+		<div className="header-right">
+		  <div className="install-login">
+			<button className="install-btn">Install Steam</button>
+			<a href="#" className="login-link">Login</a>
+		  </div>
+		  <div className="language">
+			<p className="language-text">Language</p>
+		  </div>
+		</div>
+	  </header>
+
 	);
 }
 
