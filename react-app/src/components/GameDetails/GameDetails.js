ab31d531f4a3896c5b494228856cdc3b506de098
david <david.chaotic.chu@gmail.com>
2023-06-01 22:27:41 -0400

    add load details game store.js

---
+++ react-app/src/components/GameDetails/GameDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import { getGameDetails } from "../../store/game";
+
+const GameDetails = () => {
+
+}
--- react-app/src/store/game.js
+++ react-app/src/store/game.js
@@ -1,9 +1,14 @@
 const LOAD = '/games/LOAD'
-
+const LOAD_DETAILS = '/games/LOAD_DETAILS'
 
 const load = (list) => ({
     type: LOAD,
     list
+})
+
+const loadDetails = (id) => ({
+    type: LOAD_DETAILS,
+    id
 })
 
 export const getAllGames = () => async (dispatch) => {
@@ -14,6 +19,24 @@ export const getAllGames = () => async (dispatch) => {
         dispatch(load(list.games))
     }
 }
+
+export const getGameDetails = (id) => async (dispatch) => {
+    const response = await fetch(`/api/games/${id}`);
+    if (response.ok) {
+      const game = await response.json();
+      dispatch(loadDetails(game));
+    }
+};
+
+export const getGamesByCategory = (category) => async (dispatch) => {
+    const response = await fetch(`/api/games/${category}`);
+
+    if (response.ok) {
+      const list = await response.json();
+
+      dispatch(load(list.games));
+    }
+};
 
 
 const initialState = {};
@@ -26,6 +49,8 @@ const gameReducer = (state = initialState, action) => {
                 newState[game.id] = game;
             });
             return {...newState};
+        case LOAD_DETAILS:
+            return {...state, details: action.id };
         default:
             return state;
     }
