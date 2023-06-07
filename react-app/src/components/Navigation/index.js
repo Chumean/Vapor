7788eff63c8a4523b9ba227b9a7af589061593cd
david <david.chaotic.chu@gmail.com>
2023-06-06 22:19:15 -0400

    add blueprint register in app/__init__.py

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -10,67 +10,7 @@ function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		// <div className='nav-background'>
-		// 	<div className='nav-container'>
-		// 		<div className='nav-logo'>
-
-
-		// 			<div className='vapor-logo'>
-        //             	<img src={logo} style={{ width: '3rem', height: '3rem', marginRight: '0.5rem' }} />
-        //             	<p className='vapor-text'>VAPOR</p>
-        //         	</div>
-
-		// 		</div>
-
-		// 		<div className='middle-nav'>
-        //         	<ul className='middle-list'>
-        //             	<li>
-        //                 	<p>SHOP</p>
-        //             	</li>
-
-        //             	<li>
-        //                 	<p>COMMUNITY</p>
-        //             	</li>
-
-        //             	<li>
-        //                 	<p>ABOUT</p>
-        //             	</li>
-
-        //             	<li>
-        //                 	<p>SUPPORT</p>
-        //             	</li>
-        //         	</ul>
-        //     	</div>
-
-		// 		<div className='right-nav'>
-        //         	<div className='install-logo'>
-        //             	<FaDownload />
-        //             	<p className='install-vapor'>Install Vapor</p>
-        //         	</div>
-
-        //         	<div className='login-area'>
-        //             	<p className='login-text'>login</p>
-        //         	</div>
-
-        //         	<div className='login-separate'></div>
-
-        //         	<div className=''>
-        //             	<p className='language-text'>language</p>
-        //         	</div>
-        //     	</div>
-		// 	<ul>
-		// 	<li>
-		// 		<NavLink exact to="/">Home</NavLink>
-		// 	</li>
-		// 	{isLoaded && (
-		// 		<li>
-		// 			<ProfileButton user={sessionUser} />
-		// 		</li>
-		// 	)}
-		// 	</ul>
-		// 	</div>
-		// </div>
-
+		
 		<header className="header">
 		<div className="logo">
 		  <img src={logo} alt="Vaporlogo" className="logo-img" />
