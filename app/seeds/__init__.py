4ffae522b7ec32462f8afabd582e52eb36360104
david <david.chaotic.chu@gmail.com>
2023-06-09 19:37:14 -0400

    re added seed / undo cart items

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .games import seed_games, undo_games
 from .reviews import seed_reviews, undo_reviews
+from .carts import seed_cart_items, undo_cart_items
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -17,18 +18,21 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_cart_items()
         undo_reviews()
         undo_games()
         undo_users()
     seed_users()
     seed_games()
     seed_reviews()
+    seed_cart_items()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_cart_items()
     undo_reviews()
     undo_games()
     undo_users()
