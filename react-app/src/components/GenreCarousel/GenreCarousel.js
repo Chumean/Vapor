d2e1458df17aef31d7c563c1bb1cbda64b676df1
david <david.chaotic.chu@gmail.com>
2023-08-15 23:02:21 -0400

    finish genre carousel minus css touches

--- react-app/src/components/GenreCarousel/GenreCarousel.js
+++ react-app/src/components/GenreCarousel/GenreCarousel.js
@@ -1,17 +1,17 @@
 
-import {React, useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import {React} from 'react';
+import { useSelector } from 'react-redux';
 import Slider from 'react-slick';
 import { AiOutlineLeft, AiOutlineRight} from 'react-icons/ai'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { getAllGames } from '../../store/game';
 import "./GenreCarousel.css"
+import {Link} from 'react-router-dom';
 
 const GenreCarousel = () => {
-    const dispatch = useDispatch();
     const games = useSelector(state => state.game)
-    console.log("GAMES", games)
+
 
 
 
@@ -26,6 +26,7 @@ const GenreCarousel = () => {
         autoplaySpeed: 3000,
         prevArrow: <AiOutlineLeft className="slick-prev" />,
         nextArrow: <AiOutlineRight className="slick-next" />,
+        spacing: -20
       };
 
       // useEffect(() => {
@@ -34,11 +35,11 @@ const GenreCarousel = () => {
 
 
       const genres = [
-        { key: 'rpg', url: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1685589376/656e8c37-8260-4bfc-82d0-e16650dd147c_pz6oed.jpg' },
-        { key: 'hack', url: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1687565350/featured-nier-automata-cover-art_woknln.jpg'},
-        { key: 'rogue', url: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1685590492/header_atue4i.jpg'},
-        { key: 'fighting', url: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1685590390/capsule_616x353_nxqqtx.jpg' },
-        { key: 'fps', url: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1686429471/overwatchmain_vt28ld.webp'},
+        { key: 'RPG', url: `/games/1`, image: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1685589376/656e8c37-8260-4bfc-82d0-e16650dd147c_pz6oed.jpg' },
+        { key: 'HACK N SLASH', url: `/games/2`, image: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1687565350/featured-nier-automata-cover-art_woknln.jpg'},
+        { key: 'ROGUE', url: `/games/8`, image: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1685590492/header_atue4i.jpg'},
+        { key: 'FIGHTING', url: `/games/5`, image: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1685590390/capsule_616x353_nxqqtx.jpg' },
+        { key: 'FPS', url: `/games/18`, image: 'https://res.cloudinary.com/dsu4khzr3/image/upload/v1686429471/overwatchmain_vt28ld.webp'},
       ];
 
 
@@ -48,9 +49,11 @@ const GenreCarousel = () => {
                 {genres.map((genre) => (
                 <div key={genre.key} className="genre-slide-container">
                 <div className="genre-slide">
+                <Link to={genre.url} className='genre-slide-bg'>
+                                <img src={genre.image} alt={genre.name} />
+                            </Link>
 
-
-                    <div className='genre-title'>{genre.name}</div>
+                    <div className='genre-title'>{genre.key}</div>
                 </div>
             </div>
       ))}
