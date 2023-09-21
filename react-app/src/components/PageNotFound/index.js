c3c0bbeff3923c6c282621a688daba31ad3a1363
david <david.chaotic.chu@gmail.com>
2023-09-20 21:58:03 -0400

    stlyized under construction page

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,8 +1,25 @@
+import "./notfound.css"
+import wips from "../../assets/wips.png"
+
 export default function NotFound() {
     return (
-        <div>
+        <div className="pnf">
             <h2>UNDER CONSTRUCTION</h2>
+            <img src={wips}/>
             <h2>Please go back to the homepage</h2>
+            <h2>More on me</h2>
+
+            <div className="creds">
+                <a href="https://github.com/Chumean">GitHub</a>
+            </div>
+
+            <div className="creds">
+                <a href="https://www.linkedin.com/in/davidchu007/">LinkedIn</a>
+            </div>
+
+            <div className="creds">
+                <a href="https://wellfound.com/u/david-chu-30">WellFound</a>
+            </div>
         </div>
     )
 }
