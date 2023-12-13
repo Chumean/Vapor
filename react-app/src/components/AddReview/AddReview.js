1fd3d64e5dba695b532d30f4a06b657a956de56c
david <david.chaotic.chu@gmail.com>
2023-12-12 23:14:34 -0500

    reverted back to previous commit

--- react-app/src/components/AddReview/AddReview.js
+++ react-app/src/components/AddReview/AddReview.js
@@ -1,6 +1,7 @@
 import {React , useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createReview } from "../../store/review";
+import { getGameDetails } from "../../store/game";
 import {FaRegThumbsUp, FaRegThumbsDown} from 'react-icons/fa'
 import "./AddReview.css";
 
