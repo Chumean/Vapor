73e1adbe28ba81efe193077235f53ab061f55519
david <david.chaotic.chu@gmail.com>
2024-01-10 09:41:51 -0500

    improved readability on signup form py

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -3,7 +3,6 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
-
 def user_exists(form, field):
     # Checking if user exists
     email = field.data
@@ -11,14 +10,12 @@ def user_exists(form, field):
     if user:
         raise ValidationError('Email address is already in use.')
 
-
 def username_exists(form, field):
     # Checking if username is already in use
     username = field.data
     user = User.query.filter(User.username == username).first()
     if user:
         raise ValidationError('Username is already in use.')
-
 
 class SignUpForm(FlaskForm):
     username = StringField(
