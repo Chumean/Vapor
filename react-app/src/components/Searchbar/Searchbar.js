1d35407108d2dd856562358d4c283a11ccb4e50e
david <david.chaotic.chu@gmail.com>
2024-05-07 00:12:25 -0400

    add skeleton base for searchbar js

--- react-app/src/components/Searchbar/Searchbar.js
+++ react-app/src/components/Searchbar/Searchbar.js
@@ -1,8 +1,32 @@
+import {useState} from 'react';
+import { getAllGames } from '../../store/game';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Select from 'react-select';
+
+const Searchbar = (query, games) => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [query, setQuery] = useState('');
+    const gamez = useSelector(state => state.game)
 
 
 
 
 
-// const Searchbar = () => {
+    return (
+        <div>
+            <input type="search"
+                placeholder='search'
+                onChange={(e) => setQuery(e.target.value)} />
+            <ul>
 
-// }
+            </ul>
+
+        </div>
+    )
+}
+
+export default Searchbar;
