acafd0594a7607bfce31183e6743018ff02db239
david <david.chaotic.chu@gmail.com>
2024-05-07 00:20:34 -0400

    temp mute all search bar js

--- react-app/src/components/Searchbar/Searchbar.js
+++ react-app/src/components/Searchbar/Searchbar.js
@@ -1,32 +1,45 @@
-import {useState} from 'react';
-import { getAllGames } from '../../store/game';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import Select from 'react-select';
+// import {useState} from 'react';
+// import { getAllGames } from '../../store/game';
+// import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import Select from 'react-select';
 
-const Searchbar = (query, games) => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const [query, setQuery] = useState('');
-    const gamez = useSelector(state => state.game)
+// const Searchbar = () => {
+//     const history = useHistory();
+//     const dispatch = useDispatch();
+//     const [query, setQuery] = useState('');
+//     const [results, setResults] = useState([]);
+//     const games = useSelector(state => state.game)
+
+//     useEffect(() => {
+//         dispatch(getAllGames())
+//     }, [dispatch])
+
+//     const handleChange = e => {
+//         setQuery(e.target.value)
+//     }
+
+//     useEffect(() => {
+//         if(query !== '') {
+
+//         }
+//     }, [query])
 
 
+//     return (
+//         <div>
+//             <input type="search"
+//                 placeholder='search'
+//                 className='searchbarinput'
+//                 onChange={handleChange} />
+//             <ul>
 
+//             </ul>
 
+//         </div>
+//     )
+// }
 
-    return (
-        <div>
-            <input type="search"
-                placeholder='search'
-                onChange={(e) => setQuery(e.target.value)} />
-            <ul>
-
-            </ul>
-
-        </div>
-    )
-}
-
-export default Searchbar;
+// export default Searchbar;
