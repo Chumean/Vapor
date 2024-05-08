7ed43132094020044ca9c05bf8f56d09132907a9
david <david.chaotic.chu@gmail.com>
2024-05-07 21:35:59 -0400

    remove useHistory . Will try ahref instead

--- react-app/src/components/Searchbar/Searchbar.js
+++ react-app/src/components/Searchbar/Searchbar.js
@@ -1,45 +1,49 @@
-// import {useState} from 'react';
-// import { getAllGames } from '../../store/game';
-// import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import Select from 'react-select';
+import { useState } from 'react';
+import { getAllGames } from '../../store/game';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Select from 'react-select';
 
-// const Searchbar = () => {
-//     const history = useHistory();
-//     const dispatch = useDispatch();
-//     const [query, setQuery] = useState('');
-//     const [results, setResults] = useState([]);
-//     const games = useSelector(state => state.game)
+const Searchbar = () => {
 
-//     useEffect(() => {
-//         dispatch(getAllGames())
-//     }, [dispatch])
-
-//     const handleChange = e => {
-//         setQuery(e.target.value)
-//     }
-
-//     useEffect(() => {
-//         if(query !== '') {
-
-//         }
-//     }, [query])
+    const dispatch = useDispatch();
+    const [query, setQuery] = useState('');
+    const games = useSelector(state => state.game)
+    console.log(games)
 
 
-//     return (
-//         <div>
-//             <input type="search"
-//                 placeholder='search'
-//                 className='searchbarinput'
-//                 onChange={handleChange} />
-//             <ul>
+    useEffect(() =>{
+        dispatch(getAllGames())
+    }, [dispatch])
 
-//             </ul>
+    const handleChange = e => {
+        setQuery(e.target.value)
+    }
 
-//         </div>
-//     )
-// }
 
-// export default Searchbar;
+    const handleSearch = (e) => {}
+    // useEffect(() => {
+    //     if(query !== '') {
+
+    //     }
+    // }, [query])
+
+    // a href to game detail page onClick
+    // setQuery('')
+
+    return (
+        <div>
+            <input type="search"
+                placeholder='search'
+                className='searchbarinput'
+                value={query}
+                onChange={handleChange}
+            />
+
+
+        </div>
+    )
+}
+
+export default Searchbar;
