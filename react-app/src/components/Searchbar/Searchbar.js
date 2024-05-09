aa53ae95a2ab5ba2c91c009f5d914a53a6f7bbb9
david <david.chaotic.chu@gmail.com>
2024-05-08 22:42:57 -0400

    add ul/li map for game search results

--- react-app/src/components/Searchbar/Searchbar.js
+++ react-app/src/components/Searchbar/Searchbar.js
@@ -1,49 +1,55 @@
-import { useState } from 'react';
-import { getAllGames } from '../../store/game';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import Select from 'react-select';
+// import { useState } from 'react';
+// import { getAllGames } from '../../store/game';
+// import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
-const Searchbar = () => {
-
-    const dispatch = useDispatch();
-    const [query, setQuery] = useState('');
-    const games = useSelector(state => state.game)
-    console.log(games)
-
-
-    useEffect(() =>{
-        dispatch(getAllGames())
-    }, [dispatch])
-
-    const handleChange = e => {
-        setQuery(e.target.value)
-    }
+// const Searchbar = () => {
+//     const history = useHistory();
+//     const dispatch = useDispatch();
+//     const [query, setQuery] = useState('');
+//     const games = useSelector(state => state.game)
+//     const [results, setResults] = useState([])
 
 
-    const handleSearch = (e) => {}
-    // useEffect(() => {
-    //     if(query !== '') {
 
-    //     }
-    // }, [query])
+//     useEffect(() =>{
+//         dispatch(getAllGames())
+//     }, [dispatch])
 
-    // a href to game detail page onClick
-    // setQuery('')
+//     const handleChange = e => {
+//         const searchTerm = e.target.value.toLowerCase();
+//         setQuery(searchTerm)
 
-    return (
-        <div>
-            <input type="search"
-                placeholder='search'
-                className='searchbarinput'
-                value={query}
-                onChange={handleChange}
-            />
+//         const filtered = games.filter(game =>
+//             game.title.toLowerCase().includes(searchTerm)
+//         )
+//         setResults(filtered)
+//     }
 
+//     return (
+//         <div>
+//             <input type="search"
+//                 placeholder='search'
+//                 className='searchbarinput'
+//                 value={query}
+//                 onChange={handleChange}
+//             />
+//             <ul>
+//                 {results.map(game => (
+//                     <li key={game.id}>
+//                         <Link to={`/games/${game.id}`} >
+//                             <img src={game.image} alt={game.title} />
+//                             <h2>{game.title}</h2>
+//                             <p>{game.price}</p>
+//                         </Link>
+//                     </li>
+//                 ))}
+//             </ul>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Searchbar;
+// export default Searchbar;
