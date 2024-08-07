d5aa29afbf4f148fab4f63c019c305d62c128fe5
Chumean <david.chaotic.chu@gmail.com>
2024-08-07 00:19:23 -0400

    seasrch bar import mute

--- react-app/src/components/Categories/Categories.js
+++ react-app/src/components/Categories/Categories.js
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
-import Searchbar from '../Searchbar/Searchbar'
+// import Searchbar from '../Searchbar/Searchbar'
 
 const Categories = () => {
     const history = useHistory();
