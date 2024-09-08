9a7aac5780d51f45a177167cb8f3699c44d44fdf
Chumean <david.chaotic.chu@gmail.com>
2024-09-07 21:59:15 -0400

    j Link react router dom

--- react-app/src/components/GameCard/GameCard.js
+++ react-app/src/components/GameCard/GameCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 const GameCard = (game) => {
     return (
         <div className="game-card">
