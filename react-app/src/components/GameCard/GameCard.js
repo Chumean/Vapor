50e896aedee85367990b38ce34a95d72a505d619
Chumean <david.chaotic.chu@gmail.com>
2024-09-10 22:32:22 -0400

    temp mute gamecard js

--- react-app/src/components/GameCard/GameCard.js
+++ react-app/src/components/GameCard/GameCard.js
@@ -1,23 +1,23 @@
-import React from "react";
+// import React from "react";
 
-import { Link } from "react-router-dom";
-const GameCard = (game) => {
-    return (
-        <div className="game-card">
+// import { Link } from "react-router-dom";
+// const GameCard = (game) => {
+//     return (
+//         <div className="game-card">
 
-            <div className="game-card-img"></div>
+//             <div className="game-card-img"></div>
 
-            <div className="game-card-info">
-                <div className="game-card-title"></div>
-                <div className="game-card-tags"></div>
-                <div className="game-card-reviewprice">
-                    <div className="game-card-reviews"></div>
-                    <div className="game-card-price"></div>
-                </div>
-            </div>
+//             <div className="game-card-info">
+//                 <div className="game-card-title"></div>
+//                 <div className="game-card-tags"></div>
+//                 <div className="game-card-reviewprice">
+//                     <div className="game-card-reviews"></div>
+//                     <div className="game-card-price"></div>
+//                 </div>
+//             </div>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default GameCard
+// export default GameCard
