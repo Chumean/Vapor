bb025ca5942108e86f1ea3876ad357c2fec2ac14
Chumean <david.chaotic.chu@gmail.com>
2024-11-14 23:02:55 -0500

    replace old games usestate selector

--- react-app/src/components/GameCarousel/GameCarousel.js
+++ react-app/src/components/GameCarousel/GameCarousel.js
@@ -10,7 +10,6 @@ import { AiOutlineLeft, AiOutlineRight} from 'react-icons/ai'
 const GameCarousel = ({gameLimit}) => {
 
   const games = useSelector(state => state?.game) // original
-  // const games = useSelector((state) => state?.game?.list);
 
   const settings = {
     dots: true,
