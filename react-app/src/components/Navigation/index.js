dbe7e6b42640f38cca6d03fca9ff5f833bbf93d8
Chumean <david.chaotic.chu@gmail.com>
2025-01-08 21:58:33 -0500

    revert last change

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -31,7 +31,7 @@ function Navigation({ isLoaded }){
 			<NavLink exact to="/" className="logo-link">
 
 		  		<img src={logo} alt="Vaporlogo" className="logo-img" />
-		  		<p className='vapor-text'>Vapor</p>
+		  		<p className='vapor-text'>VAPOR</p>
 			</NavLink>
 
 		</div>
